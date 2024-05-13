1164504ea5e75362b402f432973fdd512b5f723b
Fido Prayoga <fidojahfalprayoga@gmail.com>
2024-05-13 14:02:51 +0700

    Change schema for mahasiswa model in id, now id from user store on user_id

--- models/mahasiswaModel.js
+++ models/mahasiswaModel.js
@@ -1,11 +1,11 @@
 import mongoose from 'mongoose';
 
 const mahasiswaSchema = new mongoose.Schema({
-  _id: { type: mongoose.Types.ObjectId, required: true },
   nim: { type: Number, required: true },
   name: { type: String, required: true },
   jurusan: { type: String, required: true },
   telepon: { type: String, required: true },
+  user_id: { type: mongoose.Types.ObjectId, required: true, ref: 'user' },
 });
 
 const Mahasiswa = mongoose.model('mahasiswa', mahasiswaSchema);
