9bd5ee521b4cd8132af428e42dc710b8b07a3ffb
Fido Prayoga <fidojahfalprayoga@gmail.com>
2024-05-13 11:17:00 +0700

    Setup for api backend

--- app.js
+++ app.js
@@ -1,7 +1,27 @@
+import 'dotenv/config.js';
 import express from 'express';
+import cors from 'cors';
+import bodyParser from 'body-parser';
+import mongoose from 'mongoose';
 
 const app = express();
 
-app.listen(3000, () => {
-  console.log('Listening to port 3000');
+app.use(bodyParser.json());
+
+app.use(cors());
+
+app.use('/api/v1', (req, res) => {
+  return res.json({ message: 'Tes' });
 });
+
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => {
+    console.log('Connected to database');
+    app.listen(3000, () => {
+      console.log('Listening to port 3000');
+    });
+  })
+  .catch((error) => {
+    console.log('Error: ', error);
+  });
