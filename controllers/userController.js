c3391297be93ca8e20f0130e0a646f42e99efa3b
Fido Prayoga <fidojahfalprayoga@gmail.com>
2024-05-19 14:39:20 +0700

    Change response register to just have message without data and give response error when catch some error

--- controllers/userController.js
+++ controllers/userController.js
@@ -124,7 +124,7 @@ export const register = async (req, res) => {
     console.log(error);
   }
 
-  res.status(200).json({ message: 'Success', data: null });
+  res.status(200).json({ message: 'Success' });
 };
 
 export const updateUser = async (req, res) => {
