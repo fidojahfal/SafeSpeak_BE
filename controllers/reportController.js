797b9e0fd22d067fa1cc4fdf0d7e363e2682125f
Fido Prayoga <fidojahfalprayoga@gmail.com>
2024-05-22 05:38:49 +0700

    Add update report so now the report can be updated if the status is just sended not processed and can update just the status

--- controllers/reportController.js
+++ controllers/reportController.js
@@ -79,3 +79,45 @@ export const getReportById = async (req, res) => {
   }
   res.status(200).json({ message: 'Success', data: { report } });
 };
+
+export const updateReport = async (req, res) => {
+  const {
+    title,
+    type,
+    place_report,
+    date_report,
+    description,
+    evidence,
+    is_anonim,
+    status,
+  } = req.body;
+  const { report_id } = req.params;
+
+  let report;
+  try {
+    report = await Report.findById(report_id);
+  } catch (error) {
+    console.log(error);
+  }
+
+  if (report.status !== 0 && !status) {
+    return res.status(400).json({ message: 'Your report already processed!' });
+  }
+
+  try {
+    status
+      ? await Report.findByIdAndUpdate(report_id, { status })
+      : await Report.findByIdAndUpdate(report_id, {
+          title,
+          type,
+          place_report,
+          date_report,
+          description,
+          evidence,
+          is_anonim,
+        });
+  } catch (error) {
+    return console.log(error);
+  }
+  res.status(201).json({ message: 'Success', data: null });
+};
--- routes/reportRoutes.js
+++ routes/reportRoutes.js
@@ -4,6 +4,7 @@ import {
   getAllReports,
   getReportById,
   insertReport,
+  updateReport,
 } from '../controllers/reportController.js';
 
 const router = Router();
@@ -14,7 +15,7 @@ router.get('/', getAllReports);
 router.post('/', insertReport);
 
 router.get('/:report_id', getReportById);
-router.put('/:report_id', (req, res) => {});
+router.put('/:report_id', updateReport);
 
 router.get('/sum', (req, res) => {});
 
