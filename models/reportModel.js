06507740084eb8f77f96e9a029a64407f742a419
Fido Prayoga <fidojahfalprayoga@gmail.com>
2024-05-21 19:47:09 +0700

    Add reportmodels for collection reports

---
+++ models/reportModel.js
@@ -0,0 +1,18 @@
+import mongoose from 'mongoose';
+
+const reportSchema = new mongoose.Schema({
+  title: { type: String, required: true },
+  type: { type: Number, required: true },
+  place_report: { type: String, required: true },
+  date_report: { type: Date, required: true },
+  description: { type: String, required: true },
+  evidence: { type: String, required: true },
+  is_anonim: { type: Boolean, required: true },
+  user_id: { type: mongoose.Types.ObjectId, required: true, ref: 'user' },
+  status: { type: Number, required: true },
+  created_at: { type: Date, required: true, default: Date.now },
+});
+
+const Report = mongoose.model('report', reportSchema);
+
+export default Report;
