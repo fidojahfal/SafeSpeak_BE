fd91e10615212fabcf1c456b602ff7af4016a566
Fido Prayoga <fidojahfalprayoga@gmail.com>
2024-05-27 03:02:31 +0700

    Add middleware body to handle all input goes in from api

--- routes/reportRoutes.js
+++ routes/reportRoutes.js
@@ -8,6 +8,7 @@ import {
   insertReport,
   updateReport,
 } from '../controllers/reportController.js';
+import { body } from 'express-validator';
 
 const router = Router();
 
@@ -15,10 +16,34 @@ router.get('/sum', countReports);
 router.use(authToken);
 
 router.get('/', getAllReports);
-router.post('/', insertReport);
+router.post(
+  '/',
+  [
+    body('title').isLength({ min: 1, max: 255 }),
+    body('type').notEmpty(),
+    body('place_report').isLength({ min: 1, max: 255 }),
+    body('date_report').isDate(),
+    body('description').isLength({ min: 1, max: 2000 }),
+    body('evidence').notEmpty(),
+    body('is_anonim').notEmpty(),
+  ],
+  insertReport
+);
 
 router.get('/:report_id', getReportById);
-router.put('/:report_id', updateReport);
+router.put(
+  '/:report_id',
+  [
+    body('title').isLength({ min: 1, max: 255 }),
+    body('type').notEmpty(),
+    body('place_report').isLength({ min: 1, max: 255 }),
+    body('date_report').isDate(),
+    body('description').isLength({ min: 1, max: 2000 }),
+    body('evidence').notEmpty(),
+    body('is_anonim').notEmpty(),
+  ],
+  updateReport
+);
 router.delete('/:report_id', deleteReport);
 
 export default router;
