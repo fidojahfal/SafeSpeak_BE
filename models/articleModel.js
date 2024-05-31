df053c571c46c4085400fbe871e2ccbef62bf38b
Fido Prayoga <fidojahfalprayoga@gmail.com>
2024-05-31 11:07:51 +0700

    Add article model db for article feature

---
+++ models/articleModel.js
@@ -0,0 +1,12 @@
+import mongoose from 'mongoose';
+
+const articleSchema = new mongoose.Schema({
+  title: { type: String, required: true },
+  content: { type: String, required: true },
+  image: { type: String, required: true },
+  created_at: { type: Date, required: true, default: Date.now },
+});
+
+const Article = mongoose.model('article', articleSchema);
+
+export default Article;
