56da5c111266af19a920fa5f53bf84d46461228d
fidojahfal <fidojahfalprayoga@gmail.com>
2024-06-03 18:00:24 +0700

    Give article model new key so now article can be soft delete

--- models/articleModel.js
+++ models/articleModel.js
@@ -4,6 +4,7 @@ const articleSchema = new mongoose.Schema({
   title: { type: String, required: true },
   content: { type: String, required: true },
   image: { type: String, required: true },
+  is_delete: { type: Boolean, required: true },
   created_at: { type: Date, required: true, default: Date.now },
 });
 
