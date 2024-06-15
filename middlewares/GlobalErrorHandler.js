c413465ae5b4e385a49434a699a7774ae951c56d
fidojahfal <fidojahfalprayoga@gmail.com>
2024-06-15 19:08:54 +0700

    Move error global handling to standalone file to make more convenient

---
+++ middlewares/GlobalErrorHandler.js
@@ -0,0 +1,13 @@
+import multer from 'multer';
+
+function ErrorHandler(err, req, res, next) {
+  const error = err.message || 'Internal server error';
+
+  if (err instanceof multer.MulterError) {
+    return res.status(400).json({ message: 'The maximum file size is 15MB' });
+  }
+
+  res.status(500).json({ message: error });
+}
+
+export default ErrorHandler;
