fbd8df034cce64fc0d7d4d5a468b077ca3619933
fidojahfal <fidojahfalprayoga@gmail.com>
2024-06-15 18:34:15 +0700

    Give global error to handle all error goes in before controller

--- app.js
+++ app.js
@@ -21,6 +21,11 @@ app.use('/v1/users', userRouter);
 app.use('/v1/reports', reportRouter);
 app.use('/v1/articles', articleRouter);
 
+app.use((err, req, res, next) => {
+  const error = err.message || 'Internal server error';
+  res.status(500).json({ message: error });
+});
+
 mongoose
   .connect(process.env.MONGO_URI)
   .then(() => {
