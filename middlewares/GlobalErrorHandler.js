e3a3dbccac5751a2244c521f0748bfcff8921929
fidojahfal <fidojahfalprayoga@gmail.com>
2024-06-18 20:58:24 +0700

    Handling error sent header after header send

--- middlewares/GlobalErrorHandler.js
+++ middlewares/GlobalErrorHandler.js
@@ -3,6 +3,8 @@ import multer from 'multer';
 function ErrorHandler(err, req, res, next) {
   const error = err.message || 'Internal server error';
 
+  if (res.headerSent) return next(err);
+
   if (err instanceof multer.MulterError) {
     return res.status(400).json({ message: 'The maximum file size is 15MB' });
   }
